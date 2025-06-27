e92338bd25c55f06ac45b7396c2f7daaa3d3a3a3
Heitor <heitor.gb@outlook.com>
2025-06-26 21:06:29 -0300

    fix: att jest lib

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json'
-    }
-  }
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
 };
